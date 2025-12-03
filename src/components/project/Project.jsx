705764d9aa9dd42fd45d40dffdc10267b1f6fca2
Thami <thamig125@gmail.com>
2025-12-03 14:33:14 +0200

    modify project section

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,5 +1,6 @@
 import { TbWorld } from "react-icons/tb";
 import "./project-card.css";
+import { useRef } from "react";
 
 function Project({ projectInfo: data }) {
   const { name, stack } = data;
@@ -14,19 +15,47 @@ function Project({ projectInfo: data }) {
     );
   });
 
+  // Create a ref to attach to the video element
+  const videoRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+      // Optional: Reset video to the beginning
+      videoRef.current.currentTime = 0;
+    }
+  };
+
   return (
     <div className="project-card" data-aos="zoom-in">
-      <div className="card-head">{name}</div>
-      <div className="card-body">
-        <div className="card-link">
-          <a className=" link" href="#">
-            {projectInfo}
-          </a>
-        </div>
+      <div className="project-video-container" >
+        <video className="project-video"
+        ref={videoRef}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          muted
+          loop
+          preload="metadata">
+          <source src="/videos/default.mp4" type="video/mp4"></source></video>
       </div>
-      <button className="card-btn">
-        Live <TbWorld />
-      </button>
+      <div className="project-text-container">
+        <h2 className="project-title">Nextjs E-commerce</h2>
+        <p className="project-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, praesentium suscipit doloremque perferendis, deleniti possimus officia ipsum animi aperiam perspiciatis sed id vel? Quod, suscipit officiis fugiat amet cumque quidem.</p>
+        <h3 className="project-features">Key Features</h3>
+        <ul>
+          <li>Authentication</li>
+          <li>Cart Logic</li>
+          <li>Admin Dashboard</li>
+          <li>Integrated Payment gateway</li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 }
